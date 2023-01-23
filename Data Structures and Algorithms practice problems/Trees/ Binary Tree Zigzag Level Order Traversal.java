30b005a05385b4e6bb4a1ad339c97851b678f55e
Raghad Hanon <30962207+RaghadHanon@users.noreply.github.com>
2023-01-23 22:37:05 +0200

    Update and rename Data Structures and Algorithms practice problems/q.java to Data Structures and Algorithms practice problems/Trees/ Binary Tree Zigzag Level Order Traversal.java

--- Data Structures and Algorithms practice problems/Trees/ Binary Tree Zigzag Level Order Traversal.java
+++ Data Structures and Algorithms practice problems/Trees/ Binary Tree Zigzag Level Order Traversal.java
@@ -14,32 +14,37 @@
  * }
  */
 
-//https://leetcode.com/problems/binary-tree-level-order-traversal/description/?fbclid=IwAR2CHmQP24AqHhluHV_pomxKGNQzmV7tsJfXpbi9DSel-1N8GJofl9G6V04
+ //https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
 class Solution {
-  
-  List<List<Integer>>  list=new ArrayList<>();
+     List<List<Integer>>  list=new ArrayList<>();
 
     public void bfs(TreeNode root){
        Queue<TreeNode> q=new LinkedList<>();
-
-       if(root!=null){
-           q.add(root);
-       }
+       ArrayList<Integer>l=new ArrayList<>();
+       int lev=1;
+       if(root!=null)   q.add(root);
+       
        while(!q.isEmpty()){
-           int size=q.size();    
-           ArrayList<Integer>l=new ArrayList<>();
+           int size=q.size();   
+
            for(int i=0;i<size;i++){
            TreeNode node=q.remove();
            l.add(node.val);
-           if(node.left!=null)q.add(node.left);
-           if(node.right!=null)q.add(node.right);
+            if(node.left!=null)q.add(node.left);
+            if(node.right!=null)q.add(node.right);
+           
            }
-           list.add(l);
+           if(lev%2==0){
+               Collections.reverse(l);
+           }
+           list.add(new ArrayList(l));
+           l.clear();
+           lev++;
        }
 
        
     }
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         bfs(root);
         
         return list;
