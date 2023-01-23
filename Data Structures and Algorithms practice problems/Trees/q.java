e0ceb57a5902fa7cd4fcc2c2193339e168fa644a
Raghad Hanon <30962207+RaghadHanon@users.noreply.github.com>
2023-01-23 22:26:04 +0200

    Update q.java

--- Data Structures and Algorithms practice problems/Trees/q.java
+++ Data Structures and Algorithms practice problems/Trees/q.java
@@ -14,34 +14,21 @@
  * }
  */
 
-//https://leetcode.com/problems/binary-tree-level-order-traversal/description/?fbclid=IwAR2CHmQP24AqHhluHV_pomxKGNQzmV7tsJfXpbi9DSel-1N8GJofl9G6V04
+ //https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 class Solution {
-  
-  List<List<Integer>>  list=new ArrayList<>();
+    public TreeNode helper(int[] a, int left , int right) {
+     if(left>right) return null;
+     
+     int mid= (left+right)/2;
+     TreeNode root=new TreeNode(a[mid]);
 
-    public void bfs(TreeNode root){
-       Queue<TreeNode> q=new LinkedList<>();
+     root.left=helper(a,left,mid-1);
+     root.right=helper(a,mid+1,right);
+     
+     return root;
 
-       if(root!=null){
-           q.add(root);
-       }
-       while(!q.isEmpty()){
-           int size=q.size();    
-           ArrayList<Integer>l=new ArrayList<>();
-           for(int i=0;i<size;i++){
-           TreeNode node=q.remove();
-           l.add(node.val);
-           if(node.left!=null)q.add(node.left);
-           if(node.right!=null)q.add(node.right);
-           }
-           list.add(l);
-       }
-
-       
     }
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        bfs(root);
-        
-        return list;
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums,0,nums.length-1);
     }
 }
