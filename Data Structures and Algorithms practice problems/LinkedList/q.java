ef5ba0d5647bd82a38d6183607b406673e518ace
Raghad Hanon <30962207+RaghadHanon@users.noreply.github.com>
2023-01-23 23:55:16 +0200

    Update q.java

--- Data Structures and Algorithms practice problems/LinkedList/q.java
+++ Data Structures and Algorithms practice problems/LinkedList/q.java
@@ -1,47 +1,45 @@
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
+ * Definition for singly-linked list.
+ * public class ListNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 
-//https://leetcode.com/problems/binary-tree-level-order-traversal/description/?fbclid=IwAR2CHmQP24AqHhluHV_pomxKGNQzmV7tsJfXpbi9DSel-1N8GJofl9G6V04
+ //https://leetcode.com/problems/find-the-minimum-and-maximum-number-of-nodes-between-critical-points/
 class Solution {
-  
-  List<List<Integer>>  list=new ArrayList<>();
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        int[] a=new int[2];
+        if(head.next.next==null){
+           a[0]=-1; a[1]=-1;
+        return a;
+        }
+        ListNode prv=head,cur=head.next,nxt=head.next.next;
+        int initial=-1,finall=-1,previous=-1,mn=1000000,mx=0,c=0;
+        for(int i=2 ;nxt!=null;i++){
+            if((prv.val<cur.val && nxt.val < cur.val) || (prv.val>cur.val && nxt.val > cur.val)){
+                if(initial==-1)initial=i;
+                finall=i;
+                if(previous!=-1)
+                mn=Math.min(mn,i-previous);
+                previous=i;
+                c++;
+            }
+            prv=cur;
+            cur=cur.next;
+            nxt=nxt.next;
+        }
+        if(c<2){
+            a[0]=-1; a[1]=-1;
+        }else {
+        mx=finall-initial;
+        a[0]=mn;a[1]=mx;
+        }
+        return a;
 
-    public void bfs(TreeNode root){
-       Queue<TreeNode> q=new LinkedList<>();
-
-       if(root!=null){
-           q.add(root);
-       }
-       while(!q.isEmpty()){
-           int size=q.size();    
-           ArrayList<Integer>l=new ArrayList<>();
-           for(int i=0;i<size;i++){
-           TreeNode node=q.remove();
-           l.add(node.val);
-           if(node.left!=null)q.add(node.left);
-           if(node.right!=null)q.add(node.right);
-           }
-           list.add(l);
-       }
-
-       
-    }
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        bfs(root);
-        
-        return list;
+    
     }
 }
