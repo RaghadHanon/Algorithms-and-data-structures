58ada0ac65b4b7fc09efa89e4bcd65246e86eaf8
Raghad Hanon <30962207+RaghadHanon@users.noreply.github.com>
2023-01-23 22:02:44 +0200

    Add files via upload

---
+++ Data Structures and Algorithms practice problems/Trees/tq9.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ //https://leetcode.com/problems/symmetric-tree/
+class Solution {
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+    if(p==q)
+        return true;
+    
+    if(q==null || p==null || p.val!=q.val)
+        return false;
+    return isSameTree(p.left,q.right) &&  isSameTree(p.right,q.left);
+}
+
+    public boolean isSymmetric(TreeNode root) {
+       return isSameTree(root.left,root.right);
+    }
+}
