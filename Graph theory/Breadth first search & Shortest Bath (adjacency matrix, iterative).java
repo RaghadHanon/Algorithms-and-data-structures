9c571a7e9d2494e23ec593cb014c77f87dbfc552
Raghad Hanon <30962207+RaghadHanon@users.noreply.github.com>
2023-01-25 15:29:08 +0200

    Update and rename q.java to Breadth first search & Shortest Bath (adjacency matrix, iterative).java

---
+++ Graph theory/Breadth first search & Shortest Bath (adjacency matrix, iterative).java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+public class Graph {
+    int size,indx;
+    int [][]matrix;
+    Vertex[]vList;
+    int[]level;
+    int[]prv;
+
+    public Graph(int size){
+        this.size=size;
+        matrix=new int[size][size];
+        vList=new Vertex[size];
+        level=new int[size];
+        prv=new int[size];
+    }
+
+    public void addVertex(String label){
+        vList[indx++]=(new Vertex(label));
+    }
+    public void addEdgeUn(int start,int end ){
+        matrix[start][end]=1;
+        matrix[end][start]=1;
+    }
+    public void addEdgeD(int start,int end ){
+        matrix[start][end]=1;
+    }
+    public void addEdgeUnW(int start,int end ,int weight){
+        matrix[start][end]=weight;
+        matrix[end][start]=weight;
+    }
+    public void addEdgeDW(int start,int end ,int weight){
+        matrix[start][end]=weight;
+    }
+    public void PrintM (){
+        for(int i=0;i<size;i++) {
+            for (int j = 0; j < size; j++)
+                System.out.print(matrix[i][j]+ "  ");
+            System.out.println();
+        }
+    }
+    
+    public void bfs(int s) {
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(s);
+        vList[s].isVisited = true;
+        level[s]=1;
+        prv[s]=-1;
+
+        while (!q.isEmpty()) {
+            int node=q.poll();
+            System.out.print (vList[node].label+"  ");
+            for (int child=0;child<size;child++){
+                if(matrix[node][child]>0&&vList[child].isVisited==false ) {
+                    q.add(child);
+                    level[child] = level[node]+1;
+                    vList[child].isVisited=true;
+                    prv[child]=node;
+                }
+            }
+        }
+        clearVisited();
+        System.out.println();
+    }
+    public List<String > shortestPath(int start, int end){
+        bfs(start);
+
+        ArrayList<String >shPath=new ArrayList<>();
+        for(int i=end; i!=-1;i=prv[i]){
+            shPath.add(0,vList[i].label);
+        }
+        if(!shPath.get(0).equals(start))return null;
+        return shPath;
+    }
+    private void clearVisited (){
+        for(int i=0;i<size;i++)
+            vList[i].isVisited=false;
+
+    }
+
+
+
+
+}
