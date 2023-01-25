d888a27a4d0a2925722267a98d06e23a8071387d
Raghad Hanon <30962207+RaghadHanon@users.noreply.github.com>
2023-01-25 15:43:47 +0200

    Update Find the Town Judge.java

--- Data Structures and Algorithms practice problems/Graph theory/Find the Town Judge.java
+++ Data Structures and Algorithms practice problems/Graph theory/Find the Town Judge.java
@@ -1,6 +1,7 @@
 //https://leetcode.com/problems/find-the-town-judge/
 class Solution {
-    public int findJudge(int n, int[][] trust) {
+//   1.
+ public int findJudge(int n, int[][] trust) {
         int[]inDeg=new int[n+1], outDeg=new int [n+1];
        
         for(int i=0;i<trust.length;i++){
@@ -13,4 +14,22 @@ class Solution {
         }
         return -1;
     }
+//2.
+public int findJudge(int n, int[][] trust) {
+        HashMap <Integer, Integer> inDeg=new HashMap<>(), outDeg=new HashMap<>();
+        for(int i=1;i<=n;i++){
+         outDeg.put(i,0);
+         inDeg.put(i,0);
+        }
+        for(int i=0;i<trust.length;i++){
+            outDeg.put(trust[i][0],outDeg.getOrDefault(trust[i][0],0)+1);
+            inDeg.put(trust[i][1],inDeg.getOrDefault(trust[i][1],0)+1);
+        }
+        for(int i=1;i<=n;i++){
+            if(inDeg.get(i)== n-1 && outDeg.get(i)==0)
+            return i;
+        }
+        return -1;
+    }
+
 }
