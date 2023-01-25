2029c1ac3a4470d26e1946c40cd6183049ed799d
Raghad Hanon <30962207+RaghadHanon@users.noreply.github.com>
2023-01-25 13:14:44 +0200

    Update and rename q.java to Print first k Binary Number.java

---
+++ Data Structures and Algorithms practice problems/Queue/Print first k Binary Number.java
@@ -0,0 +1,16 @@
+// Print first k Binary Number
+// example : k=5
+// 1 10 11 100 101
+public static void PrintPinaryNumber(int k){
+    MyQueue<String>q=new MyQueue<>();
+    String []arr=new String[k];
+    q.add("1"); arr[0]="1";
+    for(int i=1;i<k;i+=2){
+        String temp=q.remove();
+        arr[i]=temp+"0";
+        if(i+1<k) arr[i+1]=temp+"1";
+        q.add(temp+"0");
+        q.add(temp+"1");
+    }
+    System.out.println(Arrays.toString(arr));
+}
