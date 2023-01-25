74dc000deba7326f6c3aba8b0a12dca1d228f307
Raghad Hanon <30962207+RaghadHanon@users.noreply.github.com>
2023-01-25 14:34:46 +0200

    Update and rename q.java to Ugly Number.java

---
+++ Data Structures and Algorithms practice problems/Hashing/Ugly Number.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/ugly-number/
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0)return false;
+
+       while(n>0&& n%5==0)  n/=5;
+        while(n>0&& n%3==0)  n/=3;
+         while(n>0&& n%2==0)  n/=2;
+
+         return (n==1);
+
+    }
+}
