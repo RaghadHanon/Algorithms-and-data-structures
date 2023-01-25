fbe8185adf517fd87c3a4d8d1f95f408a9b7e267
Raghad Hanon <30962207+RaghadHanon@users.noreply.github.com>
2023-01-25 13:54:46 +0200

    Update and rename q.java to Find Minimum number.java

---
+++ Data Structures and Algorithms practice problems/Stack/Find Minimum number.java
@@ -0,0 +1,17 @@
+//Find Minimum number
+
+//example : 
+// input : 101 4 5 44 66 33 2 14      output : 2
+
+public static Integer findMin( MyStack<Integer> st){
+    MyStack<Integer> copy=new MyStack<>();
+    int mn=Integer.MAX_VALUE;
+    while(!st.isEmpty()) {
+        mn=Math.min(mn,st.peek());
+        copy.push(st.pop());
+    }
+    while(!copy.isEmpty()) {
+        st.push(copy.pop());
+    }
+    return mn;
+}
